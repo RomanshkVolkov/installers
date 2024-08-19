476208b1e07161c1186c78997aa1cf1c7f45b622
Jose Guzman <joseguzmandev@gmail.com>
2024-08-19 18:23:14 +0300

    Update docker-parrot.sh
    add note other method

--- docker-parrot.sh
+++ docker-parrot.sh
@@ -1,3 +1,6 @@
+# this work on parrot os 6.1
+# wget -O - https://raw.githubusercontent.com/J7a4s0m5ine/docker-debian-installer/main/docker-debian-interactive-install.sh | bash
+
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" | \
